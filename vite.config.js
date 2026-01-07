3a31c145b5ace41f39968812135bd071ff6b2f9b
Taigours Group of Organizations <taigoursgroup@gmail.com>
2026-01-07 13:50:03 +0545

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => {
           usePolling: true,
         },
         proxy: {
-          '/api': "http://localhost:5000",
+          '/api': "http://localhost:10000",
         },
       },
       plugins: [react()],
